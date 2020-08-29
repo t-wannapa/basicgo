a3df56f6c51892c2d3fc357e589d37c1b2489c39
Wannapa Siengboon <tarn@Wannapas-MacBook-Pro.local>
2020-08-29 08:59:23 +0700

    query data.

--- myapi/database.go
+++ myapi/database.go
@@ -50,15 +50,56 @@ func getTodo() {
 	defer db.Close()
 }
 
-func main() {
-	//insertTodo()
-	//getTodo()
+func updateTodo() {
+	db, err := sql.Open("postgres", "postgres://moftatjt:KGURjLDCcP9xjQMGEvO-13prL78g2HAA@arjuna.db.elephantsql.com:5432/moftatjt")
+	if err != nil {
+		log.Fatal("connect to database error", err)
+	}
+	defer db.Close()
+	
+	stmt, err := db.Prepare("UPDATE todos SET status=$2 WHERE id=$1")
+	if err != nil {
+		log.Fatal("can't prepare statement update", err)
+	}
+	
+	if _, err := stmt.Exec(1, "inactive"); err != nil {
+		log.Fatal("error execute update", err)
+	}
+	
+	fmt.Print("error execute update", err)
+}
+
+func queryAllTodo() {
 	db, err := sql.Open("postgres", "postgres://moftatjt:KGURjLDCcP9xjQMGEvO-13prL78g2HAA@arjuna.db.elephantsql.com:5432/moftatjt")
 	if err != nil {
 		log.Fatal("connect to database error", err)
 	}
 
-	stmt, err := db.Prepare("UPDATE todos Set")
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos")
+	if err != nil {
+		log.Fatal("can't prepare query all todos statment", err)
+	}
 
-	defer db.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal("can't query all todos", err)
+	}
+
+	for rows.Next() {
+		var id int
+		var title, status string
+		err := rows.Scan(&id, &title, &status)
+		if err != nil {
+			log.Fatal("can't scan row into variable", err)
+		}
+		fmt.Println(id, title, status)
+	}
+
+	fmt.Println("query all todos success")
+}
+
+func main() {
+	//insertTodo()
+	//getTodo()
+	updateTodo()
 }
